f664188ae392c70f708939f1c1b8774a26d3d377
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-15 09:56:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,16 +13,20 @@ while answer_first != "S":
     answer_first = answer[0].upper()
 
 print("Ok, vamos jogar então!")
-print("Você tem 1 tentativas para cada pergunta")
+print("Você tem 1 tentativa para cada pergunta")
 
 print("Qual o menor e o maior país do mundo?")
 print("a) Vaticano e Rússia, b) Nauru e China, c) Mônaco e Canadá, d) Malta e Estados Unidos, e) São Marino e Índia")
 first_question = str(input("Resposta: "))
 first_question_corrected = first_question[0].upper()
-if first_question_corrected != "S":
+if first_question_corrected != "A":
     print("Alternativa Incorreta! Tente acertar a próxima!")
 else:
     print("Alternativa correta, parabéns!")
     print("Vamos para a próxima")
 
+print("Qual o livro mais vendido no mundo a seguir à Bíblia?")
+print("a) O Senhor dos Anéis, b) Dom Quixote, c) O Pequeno Príncipe, d) Ela, a Feiticeira, e) Um Conto de Duas Cidades")
 second_question = str(input("Resposta: "))
+second_question_corrected = second_question[0].upper()
+print(second_question_corrected)
