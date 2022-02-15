35ab9122aa123b5b4e5ff8759f2c5015f0c27af4
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-15 09:41:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ score = 0
 
 
 print("Seja bem Vindo Ao Quiz!")
+
+answer = str(input("Você quer jogar?"))
+answer_first = answer[0].upper()
+
+while answer_first != "S":
+    print("Tudo bem, volte quando quiser jogar!")
+    print("Seja bem Vindo Ao Quiz!")
+    answer = str(
+        input("Está afim agora de jogar?"))
