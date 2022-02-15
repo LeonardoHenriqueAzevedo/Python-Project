683344d695be428df330b195a51ce4c2c2c5d8eb
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-15 11:03:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ print("Você tem 1 tentativa para cada pergunta")
 time.sleep(3)
 print("")
 print("1 - Qual o menor e o maior país do mundo?")
-print("a) Vaticano e Rússia, b) Nauru e China, c) Mônaco e Canadá, d) Malta e Estados Unidos, e) São Marino e Índia")
+print("\033[1ma) Vaticano e Rússia, b) Nauru e China, c) Mônaco e Canadá, d) Malta e Estados Unidos, e) São Marino e Índia\033[m")
 first_question = str(input("Resposta: "))
 if first_question[0].upper() != "A":
     print("\033[1;31mAlternativa errada! Tente acertar a segunda questão.\033[m")
@@ -28,7 +28,7 @@ else:
 time.sleep(3)
 print("")
 print("2 - Qual o livro mais vendido no mundo depois da Bíblia?")
-print("a) O Senhor dos Anéis, b) Dom Quixote, c) O Pequeno Príncipe, d) Ela, a Feiticeira, e) Um Conto de Duas Cidades")
+print("\033[1ma) O Senhor dos Anéis, b) Dom Quixote, c) O Pequeno Príncipe, d) Ela, a Feiticeira, e) Um Conto de Duas Cidades\033[m")
 second_question = str(input("Resposta: "))
 if second_question[0].upper() != "B":
     print("\033[1;31mAlternativa errada! Tente acertar a terceira questão.\033[m")
@@ -40,25 +40,25 @@ else:
 time.sleep(3)
 print("")
 print("3 - Quais os países que têm a maior e a menor expectativa de vida do mundo?")
-print("a) Japão e Serra Leoa, b) Austrália e Afeganistão, c) Itália e Chade, d) Brasil e Congo, e) Estados Unidos e Angola")
+print("\033[1ma) Japão e Serra Leoa, b) Austrália e Afeganistão, c) Itália e Chade, d) Brasil e Congo, e) Estados Unidos e Angola\033[m")
 third_question = str(input("Resposta: "))
 if third_question[0].upper() != "A":
-    print("Alternativa errada! Tente acertar a quarta questão.")
+    print("\033[1;31mAlternativa errada! Tente acertar a quarta questão.\033[m")
 else:
     score += 1
-    print("Alternativa correta, parabéns!")
+    print("\033[1;32mAlternativa correta, parabéns!\033[m")
     print("Vamos para a quarta questão")
 
 time.sleep(3)
 print("")
 print("4 - Em que período da pré-história o fogo foi descoberto?")
-print("a) Neolítico, b) Paleolítico, c) Idade dos Metais, d) Período da Pedra Polida, e) Idade Média")
+print("\033[1ma) Neolítico, b) Paleolítico, c) Idade dos Metais, d) Período da Pedra Polida, e) Idade Média\033[m")
 fourth_question = str(input("Resposta: "))
 if fourth_question[0].upper() != "B":
-    print("Alternativa errada! Tente acertar a última.")
+    print("\033[1;31mAlternativa errada! Tente acertar a última.\033[m")
 else:
     score += 1
-    print("Alternativa correta, parabéns!")
+    print("\033[1;32mAlternativa correta, parabéns!\033[m")
     print("Vamos para a última questão")
 
 time.sleep(3)
@@ -67,10 +67,10 @@ print("5 - Quem pintou o teto da capela sistina?")
 print("a) Michelangelo, b) Leonardo da Vinci, c) Rafael, d) Sandro Botticelli, e) Donatello")
 fifth_question = str(input("Resposta: "))
 if fifth_question[0].upper() != "A":
-    print("Alternativa errada!")
+    print("\033[1;31mAlternativa errada!\033[m")
 else:
     score += 1
-    print("Alternativa correta, parabéns!")
+    print("\033[1;32mAlternativa correta, parabéns!\033[m")
 
 
 percentage = (score / 5) * 100
