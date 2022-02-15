67e1de2fe293537a12ed157e7ef267f1912fb789
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-15 10:21:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ print("Você tem 1 tentativa para cada pergunta")
 print("1 - Qual o menor e o maior país do mundo?")
 print("a) Vaticano e Rússia, b) Nauru e China, c) Mônaco e Canadá, d) Malta e Estados Unidos, e) São Marino e Índia")
 first_question = str(input("Resposta: "))
-first_question_corrected = first_question[0].upper()
-if first_question_corrected != "A":
+if first_question[0].upper() != "A":
     print("Alternativa errada! Tente acertar a segunda questão.")
 else:
     score += 1
@@ -28,8 +27,7 @@ else:
 print("2 - Qual o livro mais vendido no mundo depois da Bíblia?")
 print("a) O Senhor dos Anéis, b) Dom Quixote, c) O Pequeno Príncipe, d) Ela, a Feiticeira, e) Um Conto de Duas Cidades")
 second_question = str(input("Resposta: "))
-second_question_corrected = second_question[0].upper()
-if second_question_corrected != "B":
+if second_question[0].upper() != "B":
     print("Alternativa errada! Tente acertar a terceira questão.")
 else:
     score += 1
@@ -39,8 +37,7 @@ else:
 print("3 - Quais os países que têm a maior e a menor expectativa de vida do mundo?")
 print("a) Japão e Serra Leoa, b) Austrália e Afeganistão, c) Itália e Chade, d) Brasil e Congo, e) Estados Unidos e Angola")
 third_question = str(input("Resposta: "))
-third_question_corrected = third_question[0].upper()
-if third_question_corrected != "A":
+if third_question[0].upper() != "A":
     print("Alternativa errada! Tente acertar a quarta questão.")
 else:
     score += 1
@@ -50,8 +47,7 @@ else:
 print("4 - Em que período da pré-história o fogo foi descoberto?")
 print("a) Neolítico, b) Paleolítico, c) Idade dos Metais, d) Período da Pedra Polida, e) Idade Média")
 fourth_question = str(input("Resposta: "))
-fourth_question_corrected = fourth_question[0].upper()
-if fourth_question_corrected != "B":
+if fourth_question[0].upper() != "B":
     print("Alternativa errada! Tente acertar a última.")
 else:
     score += 1
@@ -61,12 +57,11 @@ else:
 print("5 - Quem pintou o teto da capela sistina?")
 print("a) Michelangelo, b) Leonardo da Vinci, c) Rafael, d) Sandro Botticelli, e) Donatello")
 fifth_question = str(input("Resposta: "))
-fifth_question_corrected = fifth_question[0].upper()
-if fifth_question_corrected != "A":
+if fifth_question[0].upper() != "A":
     print("Alternativa errada!")
 else:
     score += 1
     print("Alternativa correta, parabéns!")
 
 print("Fim Do Jogo!")
-print("Você acertou {} questões".format(score))
+print("Você acertou {} questões".format(str(score)))
