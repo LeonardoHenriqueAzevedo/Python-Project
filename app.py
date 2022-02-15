3ab952e4c2e19f813a8d9e0ab52de47111026a13
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-15 11:09:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ print("\033[1m1 - Qual o menor e o maior país do mundo?\033[m")
 print("a) Vaticano e Rússia, b) Nauru e China, c) Mônaco e Canadá, d) Malta e Estados Unidos, e) São Marino e Índia")
 first_question = str(input("Resposta: "))
 if first_question[0].upper() != "A":
-    print("\033[1;31mAlternativa errada! Tente acertar a segunda questão.\033[m")
+    print("\033[1;31mAlternativa errada!\033[m")
+    print("Tente acertar a segunda questão.")
 else:
     score += 1
     print("\033[1;32mAlternativa correta, parabéns!\033[m")
@@ -31,7 +32,8 @@ print("\033[1m2 - Qual o livro mais vendido no mundo depois da Bíblia?\033[m")
 print("a) O Senhor dos Anéis, b) Dom Quixote, c) O Pequeno Príncipe, d) Ela, a Feiticeira, e) Um Conto de Duas Cidades")
 second_question = str(input("Resposta: "))
 if second_question[0].upper() != "B":
-    print("\033[1;31mAlternativa errada! Tente acertar a terceira questão.\033[m")
+    print("\033[1;31mAlternativa errada!\033[m")
+    print("Tente acertar a terceira questão.")
 else:
     score += 1
     print("\033[1;32mAlternativa correta, parabéns!\033[m")
@@ -43,7 +45,8 @@ print("\033[1m3 - Quais os países que têm a maior e a menor expectativa de vid
 print("a) Japão e Serra Leoa, b) Austrália e Afeganistão, c) Itália e Chade, d) Brasil e Congo, e) Estados Unidos e Angola")
 third_question = str(input("Resposta: "))
 if third_question[0].upper() != "A":
-    print("\033[1;31mAlternativa errada! Tente acertar a quarta questão.\033[m")
+    print("\033[1;31mAlternativa errada!\033[m")
+    print("Tente acertar a quarta questão.")
 else:
     score += 1
     print("\033[1;32mAlternativa correta, parabéns!\033[m")
@@ -51,11 +54,12 @@ else:
 
 time.sleep(3)
 print("")
-print("4 - Em que período da pré-história o fogo foi descoberto?")
-print("\033[1ma) Neolítico, b) Paleolítico, c) Idade dos Metais, d) Período da Pedra Polida, e) Idade Média\033[m")
+print("\033[1m4 - Em que período da pré-história o fogo foi descoberto?\033[m")
+print("a) Neolítico, b) Paleolítico, c) Idade dos Metais, d) Período da Pedra Polida, e) Idade Média")
 fourth_question = str(input("Resposta: "))
 if fourth_question[0].upper() != "B":
-    print("\033[1;31mAlternativa errada! Tente acertar a última.\033[m")
+    print("\033[1;31mAlternativa errada!\033[m")
+    print("Tente acertar a última.")
 else:
     score += 1
     print("\033[1;32mAlternativa correta, parabéns!\033[m")
@@ -63,8 +67,8 @@ else:
 
 time.sleep(3)
 print("")
-print("5 - Quem pintou o teto da capela sistina?")
-print("\033[1ma) Michelangelo, b) Leonardo da Vinci, c) Rafael, d) Sandro Botticelli, e) Donatello\033[m")
+print("\033[1m5 - Quem pintou o teto da capela sistina?\033[m")
+print("a) Michelangelo, b) Leonardo da Vinci, c) Rafael, d) Sandro Botticelli, e) Donatello")
 fifth_question = str(input("Resposta: "))
 if fifth_question[0].upper() != "A":
     print("\033[1;31mAlternativa errada!\033[m")
